25e593992c43a498281533a573d7f69ea4ce464c
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2025-11-29 20:53:57 +0545

    Random Forest

---
+++ randomforest.py
@@ -0,0 +1,22 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np 
+
+X = np.array([[1], [2], [3], [4], [5], [6], [7]])
+y = np.array([0, 0, 0, 1, 1, 1, 1])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+model = RandomForestClassifier(n_estimators=70, max_depth=2)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Actual value: ", y_test)
+print("Prediction: ", y_pred)
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+
+print("Prediction for 6.5 hours:", model.predict([[6.5]]))
+
+print(model.feature_importances_)
