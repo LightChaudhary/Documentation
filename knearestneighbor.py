c9745ef9ab3ab8660bab06275bb9ae4f083be039
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2025-12-07 20:25:35 +0545

    KNearestNeighbour Classification

---
+++ knearestneighbor.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv("/Users/prakashchaudhary/Desktop/Student Performance Project/data/student_performance.csv")
+
+X = df[["Study_Hours", "Attendance", "Previous_Score"]]
+y = df['Result']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 42, stratify= y)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=11)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+print("Accuracy score: ", accuracy_score(y_pred, y_test))
+print("Classification report: ", classification_report(y_pred, y_test))
+
